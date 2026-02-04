6a3f343703bc057dae9173039973d4ab6b0ba98f
kiranreddy80 <kirankumarpilli8@gmail.com>
2026-02-04 18:32:07 +0530

    fix: update nodemailer config for better compatibility with render

--- src/contacts/contact.controller.js
+++ src/contacts/contact.controller.js
@@ -20,11 +20,17 @@ export const submitContactForm = async (req, res) => {
 
     // 2. Send Email Notification
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // You can change this to your provider
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // Use TLS
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false
+      },
+      connectionTimeout: 10000, // 10 seconds
     });
 
     const mailOptions = {
